49e211839ce689349afde46236563fea6b5fdd3d
jaguile <jaguilera126@gmail.com>
2025-04-08 14:55:31 +0200

    Test de diverses taules sobre snmpbulkwalk

--- v1-bulkwalk.py
+++ v1-bulkwalk.py
@@ -1,30 +1,9 @@
 import asyncio
 from pysnmp.hlapi.v3arch.asyncio import *
 
-"""
-Bulk walk MIB
-+++++++++++++
-
-Send a series of SNMP GETBULK requests using the following options:
-
-* with SNMPv2c, community 'public'
-* over IPv4/UDP
-* to an Agent at demo.pysnmp.com:161
-* for all OIDs past SNMPv2-MIB::system
-* run till end-of-mib condition is reported by Agent
-* based on asyncio I/O framework
-
-Functionally similar to:
-
-| $ snmpbulkwalk -v2c -c public -Cn0 -Cr50 \
-|                demo.pysnmp.com  SNMPv2-MIB::system
-
-"""  
-
 async def run():
     snmpEngine = SnmpEngine()
 
-    print ("Vaig a fer un bulk walk")
     objects = bulk_walk_cmd(
             snmpEngine,
             CommunityData("public"),
@@ -32,7 +11,8 @@ async def run():
             ContextData(),
             0,
             1,
-            ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2')),
+            # ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2')),
+            ObjectType(ObjectIdentity('.1.3.6.1.2.1.4.20')),
             lexicographicMode = False
         )
 
